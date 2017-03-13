5baa982bcdfc690d78e26742bd346c1946a6aadb
Eric Lagarda <eric@infinety.es>
2017-03-13 09:06:22 +0100

    Fix provider load Laravel 5.4

--- src/ConfigServiceProvider.php
+++ src/ConfigServiceProvider.php
@@ -8,24 +8,21 @@ class ConfigServiceProvider extends ServiceProvider
 {
     /**
      * Register the service provider.
-     *
-     * @return void
      */
     public function register()
     {
         // Bind it only once so we can reuse in IoC
-        $this->app->singleton('October\Rain\Config\Repository', function($app, $items)
-        {
+        $this->app->singleton('Infinety\Config\Repository', function ($app, $items) {
             $writer = new FileWriter($app['files'], $app['path.config']);
+
             return new Repository($items, $writer);
         });
 
         // Capture the loaded configuration items
         $config_items = app('config')->all();
 
-        $this->app['config'] = $this->app->share(function($app) use ($config_items)
-        {
-            return $app->make('October\Rain\Config\Repository', $config_items);
+        $config_items = $this->app->singleton('Infinety\Config\Repository', function ($app) use ($config_items) {
+            return $app->make('Infinety\Config\Repository', $config_items);
         });
     }
 }
